d82c104efb8802bd6f5f098702bc5fa2dc2a13a4
PaterDev <josepater_1130@hotmail.com>
2023-09-24 09:28:12 -0500

    style(Loader): Add Loader to movie id

--- src/routes/pelicula/[id]/+page.svelte
+++ src/routes/pelicula/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Loader from '../../../Components/Loader.svelte';
 	import { page } from '$app/stores';
 	let { id } = $page.params; // Recibe el parámetro
 
@@ -17,33 +18,39 @@
 		if (res.ok) {
 			return pelicula.results;
 		} else {
-			throw new Error('Hubo un error');
+			throw new Error('Película no encontrada');
 		}
 	}
 </script>
 
-<div class="row mt-5">
-	<div class="col-lg-6">
-		<img
-			src="https://image.tmdb.org/t/p/w500{pelicula.poster_path}"
-			alt={pelicula.title}
-			width="100%"
-			height="700"
-		/>
-	</div>
+{#await promesa}
+	<div class="container"><Loader /></div>
+{:then peliculas}
+	<div class="row mt-5">
+		<div class="col-lg-6">
+			<img
+				src="https://image.tmdb.org/t/p/w500{pelicula.poster_path}"
+				alt={pelicula.title}
+				width="100%"
+				height="700"
+			/>
+		</div>
 
-	<div class="col-lg-6">
-		<h1>{pelicula.title}</h1>
-		<p>{pelicula.overview}</p>
-		{#each generos as genero}
-			<h4>{genero.name}</h4>
-		{/each}
+		<div class="col-lg-6">
+			<h1>{pelicula.title}</h1>
+			<p>{pelicula.overview}</p>
+			{#each generos as genero}
+				<h4>{genero.name}</h4>
+			{/each}
 
-		<div class="rating" align="center">
-			{pelicula.vote_average}
+			<div class="rating" align="center">
+				{pelicula.vote_average}
+			</div>
 		</div>
 	</div>
-</div>
+{:catch error}
+	<p id="error">{error}</p>
+{/await}
 
 <style>
 	.rating {
@@ -51,5 +58,10 @@
 		border: solid 4px black;
 		font-size: 5rem;
 		font-weight: bolder;
+	}
+	#error {
+		color: red;
+		font-size: 30px;
+        font-weight: bold;
 	}
 </style>
