0b204e4463f0c3433693e6b6025bd0b272f579a4
PaterDev <josepater_1130@hotmail.com>
2023-09-25 01:02:22 -0500

    style(navbar): style navbar && change favicon.png

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,16 @@
+<svelte:head>
+  <title>API Películas</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
 <header>
 	<nav class="navbar navbar-expand-lg navbar-primary bg-primary">
-        <a href="/" class="navbar-brand text-white">API PELÍCULAS</a>
+        <a href="/" class="navbar-brand text-white"> 🎬 API PELÍCULAS 🎥</a>
 	</nav>
 </header>
+
+<style>
+	a {
+		font-size: 30px;
+		font-weight: bold;
+	}
+</style>
